01301c51b3787b059566d0e77093d4f8f9739f1b
César Caycho <cfabrica46@gmail.com>
2023-01-04 21:55:30 -0500

    feat: condition not extension file

--- create/create-file-list/bash.sh
+++ create/create-file-list/bash.sh
@@ -3,6 +3,10 @@
 while read -r line; do
 	if [[ $line =~ ^[0-9]. ]]; then
 		directory=$(echo "$line" | cut -d " " -f2)
-		touch "$directory"."$1"
+		if [ -z "$1" ]; then
+			touch "$directory"
+		else
+			touch "$directory"."$1"
+		fi
 	fi
 done <test.txt
