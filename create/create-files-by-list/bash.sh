9d32d47f177f804cdfba696db5591e76ece9ddb9
César Caycho <cfabrica46@gmail.com>
2023-01-05 21:23:15 -0500

    feat: create filest by list

---
+++ create/create-files-by-list/bash.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 [-f create files] [-d create directories]" 1>&2
+	exit 1
+}
+
+opt=0
+
+while getopts "fd" OPTION; do
+	case "$OPTION" in
+	f)
+		opt=1
+		;;
+	d)
+		opt=2
+		;;
+	*)
+		usage
+		exit 1
+		;;
+	esac
+done
+
+if [ "$opt" -eq 0 ]; then
+	usage
+	exit 1
+fi
+
+while read -r line; do
+	if [[ $line =~ ^[0-9]. ]]; then
+		name=$(echo "$line" | cut -d " " -f2)
+
+		if [ "$opt" -eq 1 ]; then
+			if [ -z "$3" ]; then
+				touch "$name"
+			else
+				touch "$name"."$3"
+			fi
+		else
+			mkdir "$name"
+		fi
+	fi
+done <"$2"
